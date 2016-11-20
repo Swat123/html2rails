1e726567da91e5cd7e39a91ba5f36c58283a082f
Gaurav <gaurav@techtinium.com>
2016-11-20 01:49:26 -0800

    added image conversion

--- lib/html2rails/html_converter.rb
+++ lib/html2rails/html_converter.rb
@@ -3,20 +3,47 @@ module Html2rails
   class HTMLConverter
     def initialize(file)
       html = File.exists?(file) ? open(file).read : file
-      html_doc = Nokogiri::HTML(html)
+      html_doc = Nokogiri::HTML(html,&:noblanks)
 
       traverse_recursively(html_doc)
 
-      @html = html_doc.to_html
+      html = html_doc.to_xhtml(indent:3)
+      @erb  = convert_meta_to_erb(html)
     end
 
     def to_s
-      @html
+      @erb
+    end
+
+    def convert_meta_to_erb html
+      html.split("\n").map do |line|
+        if(line.strip.start_with?('<html2rails-'))
+          CGI.unescapeHTML line.split('html2rails-txt="')[1].split('"')[0]
+        else
+          line
+        end
+      end.join("\n")
     end
 
     def traverse_recursively node
       node.children.each do |child|
         traverse_recursively child
+        case child.name
+          when 'img'
+            child.name = 'html2rails-img'
+            attributes = {}
+            child.attributes.each do |attr,value|
+              attributes[attr] = value
+              child.delete attr
+            end
+            src = attributes.delete('src')
+            if attributes.length > 0
+              child['html2rails-txt'] = "<%= image_tag(\"#{src}\", #{attributes.map{|k,v| ":"+k+" => \""+v+"\""}.join(', ')}) %>"
+            else
+              child['html2rails-txt'] = "<%= image_tag(\"#{src}\") %>"
+            end
+
+        end
       end
     end
   end
--- spec/html2rails_spec.rb
+++ spec/html2rails_spec.rb
@@ -5,14 +5,59 @@ describe Html2rails do
     expect(Html2rails::VERSION).not_to be nil
   end
 
-  it "should conver simple html without any changes" do
+  it "should convert simple html without any changes" do
     expect(Html2rails.convert!("<html><head></head><body></body></html>"))
     .to eql(
     '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-<html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=US-ASCII"></head>
-<body></body>
-</html>
-')
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII" />
+</head>
+   <body></body>
+</html>')
   end
+
+  it "should convert image tag" do
+    expect(Html2rails.convert!('<html><head></head><body><img src="/asdad.pic" /></body></html>'))
+        .to eql(
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII" />
+</head>
+   <body>
+<%= image_tag("/asdad.pic") %>
+   </body>
+</html>')
+  end
+
+
+  it "should convert image tag with additional attribute" do
+    expect(Html2rails.convert!('<html><head></head><body><img class="test-calsss" src="/asdad.pic" /></body></html>'))
+        .to eql(
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII" />
+</head>
+   <body>
+<%= image_tag("/asdad.pic", :class => "test-calsss") %>
+   </body>
+</html>')
+  end
+
+  it "should convert image tag with multiple additional attributes" do
+    expect(Html2rails.convert!('<html><head></head><body><img class="test-calsss" src="/asdad.pic" size="46"/></body></html>'))
+        .to eql(
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII" />
+</head>
+   <body>
+<%= image_tag("/asdad.pic", :class => "test-calsss", :size => "46") %>
+   </body>
+</html>')
+  end
+
 end
